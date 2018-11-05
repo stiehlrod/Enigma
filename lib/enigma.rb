e897e80d0b2cd5340a2a5664097c7438aca92b32
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-11-05 14:21:18 -0700

    test for prepare method

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -7,24 +7,34 @@ class Enigma
   end
 
   def convert_date(date_string)
-    date_string = "040895"
+    # date_string = "040895"
     date = date_string.to_i
     squared = date * date
-    squared.to_s[-4..-1].to_i
+    date_digits = squared.to_s[-4..-1]
+    date_digits.split(//).to_a
+  end
+
+  def get_the_offsets(date_string)
+    keys = convert_date(date_string)
+    keys.map do |key|
+      key.to_i
+    end
   end
 
   def character_map
     character_map = ('a'...'aa').to_a
     character_map.push(" ")
   end
-  def get_random_keys
+
+  def get_random_key
+
     random = rand(11111...99999).to_s
     random.split(//)
   end
 
-  def add_keys_together
-    # random_array = get_random_keys
-    random_array = ["1","2","3","4","5"]
+  def get_keys_array(random_array)
+    # random_array = ["1","2","3","4","5"] #testing
+
     new_array = []
     random_array.each_cons(2) do |con|
       new_array << con
@@ -33,27 +43,100 @@ class Enigma
       first+second
     end
     keys.map do |num|
-      num.to_i
+        num.to_i
     end
   end
-  # def new
-  #   binding.pry
-  #   a1 = (random_array[0] + random_array[1]).to_i
-  #   b1 = (random_array[1] + random_array[2]).to_i
-  #   c1 = (random_array[2] + random_array[3]).to_i
-  #   d1 = (random_array[3] + random_array[4]).to_i
-  #   key = convert_date(date_string)
-  #   a2 = key[0]
-  #   b2 = key[1]
-  #   c2 = key[2]
-  #   d2 = key[3]
-  #
-  #   decode_a = a1 = a2
-  #   decode_b = b1 = b2
-  #   decode_c = c1 = c2
-  #   decode_d = d1 = d2
+
+  # def make_a_hash_of_keys(array)
+  #   array = get_keys_array(array)
+  #   key_codes = {
+  #     "A": 0,
+  #     "B": 0,
+  #     "C": 0,
+  #     "D": 0
+  #   }
+  #   key_codes.each_with_index do |(key, value), i|
+  #     key_codes[key] = array[i]
+  #   end
   # end
 
-  # def encrypt(message, key, date_string)
+
+  # def combine_keys_and_offsets(date_string, key = get_random_key)
+  #   hash_of_keys = make_a_hash_of_keys(key)
+  #   offsets = get_the_offsets(date_string) #[1,0,2,5]
+  #   hash_of_keys.each_with_index do |(key,value),i|
+  #     hash_of_keys[key] = value + offsets[i]
+  #   end
   # end
+
+  def make_date_array(date_string)
+    #input ("040895")
+    #return [1,0,2,5]
+    date = date_string.to_i
+    squared = date * date
+    date_digits = squared.to_s[-4..-1]
+    date_digits.split(//).to_a
+    keys = convert_date(date_string)
+    keys.map do |key|
+      key.to_i
+    end
+  end
+
+  def make_key_array(key)
+    key_ints = key.chars.map.with_index do |char, i|
+      key[i..i+1].to_i
+    end
+    key_ints.pop
+    key_ints
+  end
+
+  def make_shift_hash(date_string, key)
+    date_ints = make_date_array(date_string)
+    key_ints = make_key_array(key)
+    shift = {}
+    date_ints.each_with_index do |int, index|
+      shift[index] = int + key_ints[index]
+    end
+    shift
+  end
+
+  def translate(char, shift_amt)#h,0
+    #find the letter int he aplha and rotate on the shift
+    new_ind = character_map.index(char) + shift_amt
+    character_map[new_ind]
+      # binding.pry
+  end
+
+  def prepare_for_encryption(message, key, date_string)
+    shift = make_shift_hash(date_string, key)
+    # binding.pry
+    message.chars.map.with_index do |char, i|
+      translate(char, shift[i % 4])
+    end.join
+
+  end
+
+#If we are trying to encrypt “hello world”, the first character
+#(“h”) is shifted by the A shift (3). So “h” becomes “k”.
+  def encrypt(message, key, date_string)
+    # binding.pry
+    message = prepare_for_encryption(message, key, date_string)
+    return_hash = {}
+    return_hash[:encryption] = message
+    return_hash[:key] = key
+    return_hash[:date] = date_string
+    return_hash
+  end
+
 end
+
+# binding.pry
+# shifts = combine_keys_and_offsets(date_string)
+# character_map
+# input = message.split(//)
+# input.each do |char|
+#   shifts.each do |key, shift|
+#     a = character_map.index(char)
+#       b = character_map.rotate(shift)
+#   end
+# end
