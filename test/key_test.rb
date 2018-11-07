00567d784c96bb779e797339e165bf23e61a73c0
Jennica Stiehl <jennica.stiehl@gmail.com>
2018-11-07 07:09:53 -0700

    adds test & method for key class

--- test/key_test.rb
+++ test/key_test.rb
@@ -5,14 +5,6 @@ require './lib/enigma'
 
 class KeyTest < Minitest::Test
 
-  def setup
-    @key = Key.new
-  end
-
-  def test_it_exists
-    assert_instance_of Key, @key
-  end
-
   def test_it_can_generate_random_key
     skip
     enigma = Enigma.new
@@ -23,17 +15,11 @@ class KeyTest < Minitest::Test
     end
   end
 
-  # def test_keys_array_can_return_an_array
-  #   skip
-  #   actual = @enigma.get_keys_array(["1","2","3","4","5"])
-  #   assert_equal [12,23,34,45], actual
-  # end
-  #
-  # def test_it_can_make_key_array
-  #   skip
-  #   expected = [02,27,71,15]
-  #   actual = @enigma.make_key_array("02715")
-  #   assert_equal expected, actual
-  # end
+  def test_it_can_make_key_array
+    enigma = Enigma.new
+    expected = [02,27,71,15]
+    actual = enigma.make_key_array("02715")
+    assert_equal expected, actual
+  end
 
 end
